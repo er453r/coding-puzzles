1e9ebf39d60c36f784e91d8a159f6034fb8622bc
Marcin Kotz <marcin@kotz.pl>
2023-12-03 09:44:05 +0100

    feat: aoc 2023 day 3

---
+++ src/com/er453r/codingpuzzles/utils/GridUtils.kt
@@ -0,0 +1,77 @@
+package com.er453r.codingpuzzles.utils
+
+import kotlin.math.abs
+import kotlin.math.max
+
+class GridCell<T>(
+    var value: T,
+    val position: Vector2d,
+)
+
+class Grid<T>(data: List<List<T>>) {
+    val data = data.mapIndexed { y, line ->
+        line.mapIndexed { x, value -> GridCell(value, Vector2d(x, y)) }
+    }
+
+    val width = data.first().size
+    val height = data.size
+
+    fun get(x: Int, y: Int) = data[y][x]
+    operator fun get(vector2d: Vector2d) = get(vector2d.x, vector2d.y)
+
+    fun contains(x: Int, y: Int) = (x in 0 until width) && (y in 0 until height)
+
+    operator fun contains(vector2d: Vector2d) = contains(vector2d.x, vector2d.y)
+
+    fun crossNeighbours(vector2d: Vector2d) = Vector2d.DIRECTIONS.map { vector2d + it }.filter { contains(it) }.map { get(it) }
+    fun getAll(vectors: Set<Vector2d>) = vectors.filter { contains(it) }.map { get(it) }
+}
+
+
+data class Vector2d(var x: Int = 0, var y: Int = 0) {
+    companion object {
+        val UP = Vector2d(0, -1)
+        val DOWN = Vector2d(0, 1)
+        val LEFT = Vector2d(-1, -0)
+        val RIGHT = Vector2d(1, 0)
+        val DIRECTIONS = arrayOf(UP, DOWN, LEFT, RIGHT)
+    }
+
+    operator fun plus(vector2d: Vector2d) = Vector2d(x + vector2d.x, y + vector2d.y)
+    operator fun minus(vector2d: Vector2d) = Vector2d(x - vector2d.x, y - vector2d.y)
+
+    fun increment(vector2d: Vector2d): Vector2d {
+        this.x += vector2d.x
+        this.y += vector2d.y
+
+        return this
+    }
+
+    fun normalized() = Vector2d(if (x != 0) x / abs(x) else 0, if (y != 0) y / abs(y) else 0)
+
+    fun negative() = Vector2d(-x, -y)
+
+    fun length() = max(abs(x), abs(y))
+
+    fun manhattan() = abs(x) + abs(y)
+
+    fun neighbours8() = setOf(
+        this + UP,
+        this + UP + LEFT,
+        this + UP + RIGHT,
+        this + LEFT,
+        this + RIGHT,
+        this + DOWN,
+        this + DOWN + LEFT,
+        this + DOWN + RIGHT,
+    )
+
+    fun neighboursTopBottom() = setOf(
+        this + UP,
+        this + UP + LEFT,
+        this + UP + RIGHT,
+        this + DOWN,
+        this + DOWN + LEFT,
+        this + DOWN + RIGHT,
+    )
+}
--- test/com/er453r/codingpuzzles/aoc/aoc2023/Day03.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2023/Day03.kt
@@ -1,22 +1,99 @@
 package com.er453r.codingpuzzles.aoc.aoc2023
 
 import com.er453r.codingpuzzles.aoc.AoCTestBase
+import com.er453r.codingpuzzles.utils.Grid
+import com.er453r.codingpuzzles.utils.GridCell
+import com.er453r.codingpuzzles.utils.Vector2d
 import org.junit.jupiter.api.DisplayName
 
 @DisplayName("AoC 2023 - Day 03")
 class Day03 : AoCTestBase<Int>(
     year = 2023,
     day = 3,
-    testTarget1 = null,
-    puzzleTarget1 = null,
-    testTarget2 = null,
-    puzzleTarget2 = null,
+    testTarget1 = 4361,
+    puzzleTarget1 = 539713,
+    testTarget2 = 467835,
+    puzzleTarget2 = 84159075,
 ) {
     override fun part1(input: List<String>): Int {
-        return 0
+        val grid = Grid(input.map { it.toCharArray().toList() })
+
+        var sum = 0;
+
+        for(y in 0 until grid.height){
+            var partNumber = ""
+            var adjacent = false
+
+            for(x in 0 until grid.width) {
+                val cell = grid.get(x, y)
+
+                if(cell.value.isDigit()) {
+                    partNumber += cell.value
+
+                    if(!adjacent)
+                        adjacent = grid.getAll(cell.position.neighbours8()).any { it.value != '.' && !it.value.isDigit() }
+                }
+
+                if((!cell.value.isDigit() || x == (grid.width - 1)) && partNumber.length > 0) {
+                    if(adjacent)
+                        sum += partNumber.toInt()
+
+                    adjacent = false
+                    partNumber = ""
+                }
+            }
+        }
+
+        return sum
     }
 
     override fun part2(input: List<String>): Int {
-        return 0
+        val grid = Grid(input.map { it.toCharArray().toList() })
+
+        val parts = mutableSetOf<Pair<Int, Set<Vector2d>>>();
+
+        for(y in 0 until grid.height){
+            var partNumber = ""
+            var adjacent = false
+            var neighbours = mutableSetOf<Vector2d>()
+
+            for(x in 0 until grid.width) {
+                val cell = grid.get(x, y)
+
+                if(cell.value.isDigit()) {
+                    partNumber += cell.value
+                    neighbours += cell.position.neighbours8()
+
+                    if(!adjacent)
+                        adjacent = grid.getAll(cell.position.neighbours8()).any { it.value != '.' && !it.value.isDigit() }
+                }
+
+                if((!cell.value.isDigit() || x == (grid.width - 1)) && partNumber.length > 0) {
+                    if(adjacent)
+                        parts.add(Pair(partNumber.toInt(), neighbours))
+
+                    adjacent = false
+                    partNumber = ""
+                    neighbours = mutableSetOf()
+                }
+            }
+        }
+
+        var sum = 0
+
+        for(y in 0 until grid.height){
+            for(x in 0 until grid.width) {
+                val cell = grid.get(x, y)
+
+                if(cell.value == '*'){
+                    val neighbourParts = parts.filter { it.second.contains(cell.position) }
+
+                    if(neighbourParts.size == 2)
+                        sum += neighbourParts.first().first * neighbourParts.last().first
+                }
+            }
+        }
+
+        return sum
     }
 }

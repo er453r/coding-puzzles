eea0dcb69d91686e5e6f34711c848fd24d169d33
Marcin Kotz <marcin@kotz.pl>
2023-12-06 08:41:23 +0100

    feat: aoc 2023 day 6

--- test/com/er453r/codingpuzzles/aoc/aoc2023/Day06.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2023/Day06.kt
@@ -1,22 +1,33 @@
 package com.er453r.codingpuzzles.aoc.aoc2023
 
 import com.er453r.codingpuzzles.aoc.AoCTestBase
+import com.er453r.codingpuzzles.utils.longs
 import org.junit.jupiter.api.DisplayName
 
 @DisplayName("AoC 2023 - Day 06")
-class Day06 : AoCTestBase<Int>(
+class Day06 : AoCTestBase<Long>(
     year = 2023,
     day = 6,
-    testTarget1 = null,
-    puzzleTarget1 = null,
-    testTarget2 = null,
-    puzzleTarget2 = null,
+    testTarget1 = 288,
+    puzzleTarget1 = 393120,
+    testTarget2 = 71503,
+    puzzleTarget2 = 36872656,
 ) {
-    override fun part1(input: List<String>): Int {
-        return 0
-    }
+    private fun solve(raceTimes: List<Long>, recordDistances: List<Long>) = raceTimes.mapIndexed { index, raceTime ->
+        val recordDistance = recordDistances[index]
 
-    override fun part2(input: List<String>): Int {
-        return 0
-    }
+        LongRange(0, raceTime).map { dt ->
+            (raceTime - dt) * dt > recordDistance  // (time left) * velocity
+        }.count { it }.toLong()
+    }.reduce(Long::times)
+
+    override fun part1(input: List<String>) = solve(
+        raceTimes = input.first().longs(),
+        recordDistances = input.last().longs(),
+    )
+
+    override fun part2(input: List<String>) = solve(
+        raceTimes = input.first().replace(" ", "").longs(),
+        recordDistances = input.last().replace(" ", "").longs(),
+    )
 }

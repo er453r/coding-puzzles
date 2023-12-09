bc55f0dab596b94e257ab1dcce47d77c8e732d56
Marcin Kotz <marcin@kotz.pl>
2023-12-09 07:11:04 +0100

    feat: aoc 2023 day 9

--- test/com/er453r/codingpuzzles/aoc/aoc2023/Day09.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2023/Day09.kt
@@ -1,22 +1,47 @@
 package com.er453r.codingpuzzles.aoc.aoc2023
 
 import com.er453r.codingpuzzles.aoc.AoCTestBase
+import com.er453r.codingpuzzles.utils.longs
 import org.junit.jupiter.api.DisplayName
 
 @DisplayName("AoC 2023 - Day 09")
-class Day09 : AoCTestBase<Int>(
+class Day09 : AoCTestBase<Long>(
     year = 2023,
     day = 9,
-    testTarget1 = null,
-    puzzleTarget1 = null,
-    testTarget2 = null,
-    puzzleTarget2 = null,
+    testTarget1 = 114,
+    puzzleTarget1 = 1992273652,
+    testTarget2 = 2,
+    puzzleTarget2 = 1012,
 ) {
-    override fun part1(input: List<String>): Int {
-        return 0
+    private fun solve(input: List<String>, predict: (MutableList<MutableList<Long>>) -> Long) = input.map { it.longs() }.sumOf { numbers ->
+        val history = mutableListOf(numbers.toMutableList())
+        var previous = numbers
+
+        while (previous.any { it != 0L }) {
+            val reduced = (0..previous.size - 2).map { previous[it + 1] - previous[it] }
+            history.add(reduced.toMutableList())
+            previous = reduced
+        }
+
+        predict(history)
     }
 
-    override fun part2(input: List<String>): Int {
-        return 0
+    override fun part1(input: List<String>) = solve(input) { history ->
+        history.last().add(0)
+
+        for (n in history.size - 2 downTo 0)
+            history[n].add(history[n].last() + history[n + 1].last())
+
+        history.first().last()
+    }
+
+
+    override fun part2(input: List<String>) = solve(input) { history ->
+        history.last().addFirst(0)
+
+        for (n in history.size - 2 downTo 0)
+            history[n].addFirst(history[n].first() - history[n + 1].first())
+
+        history.first().first()
     }
 }

03a4e53a64cc85e51702c41b7c9fdb3502c3397c
Marcin Kotz <marcin@kotz.pl>
2023-12-12 16:00:57 +0100

    feat: aoc 2023 day 12, part 1, better pruning

--- test/com/er453r/codingpuzzles/aoc/aoc2023/Day12.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2023/Day12.kt
@@ -13,72 +13,52 @@ class Day12 : AoCTestBase<Int>(
     testTarget2 = 525152,
     puzzleTarget2 = null,
 ) {
-    private fun damagedGroups(line: String) = "(#+)".toRegex().findAll(line).map { it.value.length }.toList()
+    private fun countRow(row: List<Char>, groups: List<Int>): Int {
+        val groupsString = groups.toString()
+        val groupsSum = groups.sum()
+        val maxGroup = groups.max()
+        val okSum = row.size - groupsSum
 
+        val stack = mutableListOf(row)
+        val ok = mutableListOf<List<Char>>()
 
-    private fun addVariants(string: String, maxLength: Int, ok: Int, damaged: Int): List<String> {
-        if (string.length == maxLength)
-            return listOf(string)
+        val gRegex = "(#+)".toRegex()
 
-        if (string.count { it == '#' } == damaged)
-            return listOf(string + ".".repeat(maxLength - string.length))
+        while (stack.isNotEmpty()) {
+            val top = stack.removeLast()
+            val firstUnknown = top.indexOfFirst { it == '?' }
 
-        if (string.count { it == '.' } == ok)
-            return listOf(string + "#".repeat(maxLength - string.length))
+            val topGroups = gRegex.findAll(top.joinToString("")).map { it.value.length }.toList()
 
-        return addVariants("$string.", maxLength, ok, damaged) + addVariants("$string#", maxLength, ok, damaged)
-    }
-
-    private fun gapsGroupsToString(gaps: List<Int>, groups: List<Int>): String {
-        return List(groups.size) { index ->
-            ".".repeat(gaps[index]) + "#".repeat(groups[index])
-        }.joinToString("") + ".".repeat(gaps.last())
-    }
-
-    private fun gapPermutations(gaps: List<Int>, toGive: Int, index: Int = 0): List<List<Int>> {
-        if (index > gaps.size - 1)
-            return emptyList()
-
-        if (toGive == 0)
-            return listOf(gaps)
-
-        return (toGive downTo 0).flatMap {
-            val newList = gaps.toMutableList()
-            newList[index] += it
-
-            gapPermutations(newList, toGive - it, index + 1)
+            if (firstUnknown == -1){
+                if(topGroups.toString() == groupsString)
+                    ok.add(top)
+                else
+                    continue
+                }
+            else if(top.count { it == '#' } > groupsSum)
+                continue
+            else if(top.count { it == '.' } > okSum)
+                continue
+//            else if(topGroups.size > groups.size )
+//                continue
+//            else if(topGroups.indices.indexOfFirst { topGroups[it] > groups[it] } != -1)
+//                continue
+            else {
+                stack.add(top.toMutableList().also { l -> l[firstUnknown] = '#' })
+                stack.add(top.toMutableList().also { l -> l[firstUnknown] = '.' })
+            }
         }
+
+        return ok.size
     }
 
     override fun part1(input: List<String>): Int {
         return input.sumOf { line ->
-            val r = line.split(" ").first()
-            val g = line.ints()
-            val row = r
-            val groups = g
-            val groupsString = groups.toString()
+            val row = line.split(" ").first().toCharArray().toList()
+            val groups = line.ints()
 
-            val unknownIndexes = row.indices.filter { row[it] == '?' }
-            val maxDamaged = groups.sum()
-            val maxKnownDamaged = row.indices.filter { row[it] == '#' }.count()
-            val unknownDamaged = maxDamaged - maxKnownDamaged
-            val unknownOk = unknownIndexes.size - unknownDamaged
-            val maxOK = row.length - maxDamaged
-
-            val variants = addVariants("", unknownIndexes.size, unknownOk, unknownDamaged)
-
-            variants.map { variant ->
-                val realString = row.indices.map {
-                    if (it in unknownIndexes)
-                        variant[unknownIndexes.indexOf(it)]
-                    else
-                        row[it]
-                }.joinToString("")
-
-                val damaged = damagedGroups(realString)
-
-                damaged.toString()
-            }.count { it == groupsString }
+            countRow(row, groups)
         }
     }
 
@@ -86,31 +66,10 @@ class Day12 : AoCTestBase<Int>(
         return input.sumOf { line ->
             val r = line.split(" ").first()
             val g = line.ints()
-            val row = r + r + r + r
-            val groups = g + g + g + g + g
-            val groupsString = groups.toString()
+            val row = (r + r + r + r + r).toCharArray().toList()
+            val groups = (g + g + g + g + g)
 
-            val unknownIndexes = row.indices.filter { row[it] == '?' }
-            val maxDamaged = groups.sum()
-            val maxKnownDamaged = row.indices.filter { row[it] == '#' }.count()
-            val unknownDamaged = maxDamaged - maxKnownDamaged
-            val unknownOk = unknownIndexes.size - unknownDamaged
-            val maxOK = row.length - maxDamaged
-
-            val variants = addVariants("", unknownIndexes.size, unknownOk, unknownDamaged)
-
-            variants.map { variant ->
-                val realString = row.indices.map {
-                    if (it in unknownIndexes)
-                        variant[unknownIndexes.indexOf(it)]
-                    else
-                        row[it]
-                }.joinToString("")
-
-                val damaged = damagedGroups(realString)
-
-                damaged.toString()
-            }.count { it == groupsString }
+            countRow(row, groups)
         }
     }
 }

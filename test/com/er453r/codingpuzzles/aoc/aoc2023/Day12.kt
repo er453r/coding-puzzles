081e2292419bd468cd784b88a8e359dd1b90569a
Marcin Kotz <marcin@kotz.pl>
2023-12-12 20:45:58 +0100

    feat: aoc 2023 day 12, part 2 passes test, pruning other way

--- test/com/er453r/codingpuzzles/aoc/aoc2023/Day12.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2023/Day12.kt
@@ -14,52 +14,82 @@ class Day12 : AoCTestBase<Int>(
     testTarget2 = 525152,
     puzzleTarget2 = null,
 ) {
-    private fun countRow(row: List<Char>, groups: List<Int>): Int {
+    private fun countRow(row: String, groups: List<Int>): Int {
         val groupsString = groups.toString()
-        val groupsSum = groups.sum()
-        val maxGroup = groups.max()
-        val okSum = row.size - groupsSum
-
         val stack = mutableListOf(row)
-        val ok = mutableListOf<List<Char>>()
+        var ok = 0
 
         val gRegex = "(#+)".toRegex()
 
         while (stack.isNotEmpty()) {
             val top = stack.removeLast()
+
+            if(top.length != row.length)
+                println("Something is yesno!")
+
             val firstUnknown = top.indexOfFirst { it == '?' }
 
-            val topGroups = gRegex.findAll(top.joinToString("")).map { it.value.length }.toList()
-            val topGroupsKnown = gRegex.findAll(top.joinToString("").substring(0, max(firstUnknown, 0))).map { it.value.length }.toList()
+            val topGroups = gRegex.findAll(top).map { it.value.length }.toList()
 
-            if (firstUnknown == -1){
-                if(topGroups.toString() == groupsString)
-                    ok.add(top)
+            if (firstUnknown == -1) {
+                if (topGroups.toString() == groupsString)
+                    ok++
                 else
                     continue
-                }
-            else if(top.count { it == '#' } > groupsSum)
-                continue
-            else if(top.count { it == '.' } > okSum)
-                continue
-            else if(topGroupsKnown.size > groups.size )
-                continue
-            else if(topGroupsKnown.indices.indexOfFirst { topGroupsKnown[it] > groups[it] } != -1)
-                continue
-            else {
-                stack.add(top.toMutableList().also { l -> l[firstUnknown] = '#' })
-                stack.add(top.toMutableList().also { l -> l[firstUnknown] = '.' })
+            } else {
+                val topGroupsKnown = gRegex.findAll(top.substring(0, max(firstUnknown, 0))).map { it.value.length }.toList()
+                val previous = if (firstUnknown > 1) top[firstUnknown - 1] else ""
+
+//                if(topGroupsKnown.indices.any { topGroupsKnown[it] > groups[it] })
+//                    continue
+//                else if (previous == '#') {
+//                    val leftInGroup = groups[topGroupsKnown.size - 1] - topGroupsKnown.last()
+//                    val filledGroup = top.replaceRange(firstUnknown, firstUnknown + leftInGroup, "#".repeat(leftInGroup))
+//
+//                    if(firstUnknown + leftInGroup + 1 < filledGroup.length)
+//                        stack.add(filledGroup.replaceRange(firstUnknown + leftInGroup, firstUnknown + leftInGroup + 1, "."))
+//                    else
+//                        stack.add(filledGroup)
+//
+//                    if(stack.takeLast(1).any { it.length != row.length })
+//                        println("Something is yesno!")
+//                } else if (previous == '.') {
+//                    val hasNextGroup = topGroupsKnown.size < groups.size
+//
+//                    if(!hasNextGroup) {
+//                        stack.add(top.replaceRange(firstUnknown, top.length, ".".repeat(top.length + 1 - firstUnknown)))
+//
+//                        if(stack.takeLast(1).any { it.length != row.length })
+//                            println("Something is yesno!")
+//                    }
+//                    else{
+//                        val nextGroup = groups[topGroupsKnown.size + 1]
+//
+//                        stack.add(top.replaceRange(firstUnknown, firstUnknown + nextGroup, "#".repeat(nextGroup)))
+//                        stack.add(top.replaceRange(firstUnknown, firstUnknown + 1, "."))
+//
+//                        if(stack.takeLast(2).any { it.length != row.length })
+//                            println("Something is yesno!")
+//                    }
+//                }
+//                else {
+                    stack.add(top.replaceRange(firstUnknown, firstUnknown + 1, "#"))
+                    stack.add(top.replaceRange(firstUnknown, firstUnknown + 1, "."))
+
+                    if(stack.takeLast(1).any { it.length != row.length })
+                        println("Something is yesno!")
+//                }
             }
         }
 
-//        println(ok.size)
+//        println("$row ${ok.size}")
 
-        return ok.size
+        return ok
     }
 
     override fun part1(input: List<String>): Int {
         return input.sumOf { line ->
-            val row = line.split(" ").first().toCharArray().toList()
+            val row = line.split(" ").first()
             val groups = line.ints()
 
             countRow(row, groups)
@@ -70,8 +100,8 @@ class Day12 : AoCTestBase<Int>(
         return input.sumOf { line ->
             val r = line.split(" ").first()
             val g = line.ints()
-            val row = (r + "?" + r + "?" + r + "?" + r + "?" + r).toCharArray().toList()
-            val groups = (g + g + g + g + g)
+            val row = (0 until 5).joinToString("?") { r }
+            val groups = (0 until 5).flatMap { g }
 
             countRow(row, groups)
         }

ddfe38806c31c9720b08ce5094e1170d77bb953f
Marcin Kotz <marcin@kotz.pl>
2023-12-14 06:40:47 +0100

    feat: aoc 2023 day 14

--- test/com/er453r/codingpuzzles/aoc/aoc2023/Day14.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2023/Day14.kt
@@ -1,22 +1,142 @@
 package com.er453r.codingpuzzles.aoc.aoc2023
 
 import com.er453r.codingpuzzles.aoc.AoCTestBase
+import com.er453r.codingpuzzles.utils.Grid
 import org.junit.jupiter.api.DisplayName
 
 @DisplayName("AoC 2023 - Day 14")
 class Day14 : AoCTestBase<Int>(
     year = 2023,
     day = 14,
-    testTarget1 = null,
-    puzzleTarget1 = null,
-    testTarget2 = null,
-    puzzleTarget2 = null,
+    testTarget1 = 136,
+    puzzleTarget1 = 108759,
+    testTarget2 = 64,
+    puzzleTarget2 = 89089,
 ) {
+    fun tiltN(grid: Grid<Char>) {
+        grid.columns().forEach { column ->
+            while (true) {
+                val canFallDown = column.indices.indexOfFirst { it > 0 && column[it].value == 'O' && column[it - 1].value == '.' }
+
+                if (canFallDown == -1)
+                    break
+                else {
+                    column[canFallDown].value = '.'
+                    column[canFallDown - 1].value = 'O'
+                }
+            }
+        }
+    }
+
+    fun tiltS(grid: Grid<Char>) {
+        grid.columns().forEach { column ->
+            while (true) {
+                val canFallDown = column.indices.reversed().firstOrNull { it < column.size - 1 && column[it].value == 'O' && column[it + 1].value == '.' }
+
+                if (canFallDown == null)
+                    break
+                else {
+                    column[canFallDown].value = '.'
+                    column[canFallDown + 1].value = 'O'
+                }
+            }
+        }
+    }
+
+    fun tiltW(grid: Grid<Char>) {
+        grid.rows().forEach { row ->
+            while (true) {
+                val canFallDown = row.indices.indexOfFirst { it > 0 && row[it].value == 'O' && row[it - 1].value == '.' }
+
+                if (canFallDown == -1)
+                    break
+                else {
+                    row[canFallDown].value = '.'
+                    row[canFallDown - 1].value = 'O'
+                }
+            }
+        }
+    }
+
+    fun tiltE(grid: Grid<Char>) {
+        grid.rows().forEach { row ->
+            while (true) {
+                val canFallDown = row.indices.reversed().firstOrNull { it < row.size - 1 && row[it].value == 'O' && row[it + 1].value == '.' }
+
+                if (canFallDown == null)
+                    break
+                else {
+                    row[canFallDown].value = '.'
+                    row[canFallDown + 1].value = 'O'
+                }
+            }
+        }
+    }
+
+    fun cycle(grid: Grid<Char>) {
+        tiltN(grid)
+        tiltW(grid)
+        tiltS(grid)
+        tiltE(grid)
+    }
+
+    fun score(grid: Grid<Char>):Int{
+        return grid.columns().map { column ->
+            column.filter { it.value == 'O' }
+                .map { column.size - it.position.y }
+                .sum()
+        }.sum()
+    }
+
     override fun part1(input: List<String>): Int {
-        return 0
+        val grid = Grid(input.map { it.toCharArray().toList() })
+
+        tiltN(grid);
+
+        return score(grid)
+    }
+
+    fun <T> List<T>.findLongestSequence(): Pair<Int, Int> {
+        val sequences = mutableListOf<Pair<Int, Int>>()
+
+        for (startPos in indices) {
+            for (sequenceLength in 1..(this.size - startPos) / 2) {
+                var sequencesAreEqual = true
+
+                for (i in 0 until sequenceLength)
+                    if (this[startPos + i] != this[startPos + sequenceLength + i]) {
+                        sequencesAreEqual = false
+                        break
+                    }
+
+                if (sequencesAreEqual)
+                    sequences += Pair(startPos, sequenceLength)
+            }
+        }
+
+        return sequences.maxBy { it.second }
     }
 
     override fun part2(input: List<String>): Int {
-        return 0
+        val grid = Grid(input.map { it.toCharArray().toList() })
+
+        val points = mutableListOf<Int>()
+
+        for(n in 0 until 200) {
+            cycle(grid)
+
+            points.add(score(grid))
+        }
+
+        val bestSequence = points.findLongestSequence()
+        val iters = 1000000000
+        val sequence = points.subList(bestSequence.first, bestSequence.first + bestSequence.second)
+        val sequenceStartIndex = bestSequence.first
+        var index = sequenceStartIndex
+
+        while(index + sequence.size < iters)
+            index += sequence.size
+
+        return sequence[iters - index - 1]
     }
 }

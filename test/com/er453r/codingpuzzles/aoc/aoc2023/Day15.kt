89952cc6db22a39908ed221ab453edf2aff6a640
Marcin Kotz <marcin@kotz.pl>
2023-12-15 08:12:45 +0100

    feat: refactored day 15

--- test/com/er453r/codingpuzzles/aoc/aoc2023/Day15.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2023/Day15.kt
@@ -12,55 +12,39 @@ class Day15 : AoCTestBase<Int>(
     testTarget2 = 145,
     puzzleTarget2 = 243747,
 ) {
-    fun hash(string: String):Int{
-        var sum = 0
-
-        string.forEach {
-            sum += it.code
-            sum *= 17
-            sum %= 256
-        }
-
-        return sum
+    private fun hash(string: String) = string.fold(0) { sum, it ->
+        ((sum + it.code) * 17) % 256
     }
 
-    override fun part1(input: List<String>): Int {
-        val steps = input.first().split(",")
-
-        return steps.map { hash(it) }.sum()
-    }
+    override fun part1(input: List<String>) = input.first().split(",").sumOf { hash(it) }
 
     override fun part2(input: List<String>): Int {
-        val boxes = MutableList(256){ mutableListOf<String>() }
-        val steps = input.first().split(",")
+        val boxes = MutableList(256) { mutableListOf<String>() }
 
-        steps.forEach { step ->
-            if(step.endsWith("-")){
-                val (label,_) = step.split("-")
+        input.first().split(",").forEach { step ->
+            if (step.endsWith("-")) {
+                val (label, _) = step.split("-")
                 val box = hash(label)
 
                 boxes[box].removeIf {
                     it.startsWith(label)
                 }
-            }
-            else{
+            } else {
                 val (label, focus) = step.split("=")
                 val box = hash(label)
-
                 val key = "$label $focus"
+                val indexOfFirst = boxes[box].indexOfFirst { it.startsWith(label) }
 
-                val existing = boxes[box].indexOfFirst { it.startsWith(label) }
-
-                if(existing == -1)
+                if (indexOfFirst == -1)
                     boxes[box].add(key)
                 else
-                    boxes[box][existing] = key
+                    boxes[box][indexOfFirst] = key
             }
         }
 
         return boxes.flatMapIndexed { boxIndex, box ->
-            box.mapIndexed { lensIndex, lens  ->
-                val (label, focus) = lens.split(" ")
+            box.mapIndexed { lensIndex, lens ->
+                val (_, focus) = lens.split(" ")
 
                 (boxIndex + 1) * (lensIndex + 1) * focus.toInt()
             }

b55f7c2d159a09a51062f45d578adb06938f385e
Marcin Kotz <marcin@kotz.pl>
2023-12-18 01:53:41 +0100

    feat: template for aoc2023 day 18

---
+++ test/com/er453r/codingpuzzles/aoc/aoc2023/Day18.kt
@@ -0,0 +1,22 @@
+package com.er453r.codingpuzzles.aoc.aoc2023
+
+import com.er453r.codingpuzzles.aoc.AoCTestBase
+import org.junit.jupiter.api.DisplayName
+
+@DisplayName("AoC 2023 - Day 18")
+class Day18 : AoCTestBase<Int>(
+    year = 2023,
+    day = 18,
+    testTarget1 = null,
+    puzzleTarget1 = null,
+    testTarget2 = null,
+    puzzleTarget2 = null,
+) {
+    override fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    override fun part2(input: List<String>): Int {
+        return 0
+    }
+}

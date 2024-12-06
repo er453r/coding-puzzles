c38a77bd43e3d91bd7ac4407306491c8a14b71e1
Marcin Kotz <marcin@kotz.pl>
2024-12-06 09:08:12 +0100

    feat: aoc2024 day 6 and day 5 refactor

--- test/com/er453r/codingpuzzles/aoc/aoc2024/Day05.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2024/Day05.kt
@@ -3,7 +3,6 @@ package com.er453r.codingpuzzles.aoc.aoc2024
 import com.er453r.codingpuzzles.aoc.AoCTestBase
 import com.er453r.codingpuzzles.utils.ints
 import org.junit.jupiter.api.DisplayName
-import kotlin.math.max
 import kotlin.math.min
 
 @DisplayName("AoC 2024 - Day 05")
@@ -13,88 +12,50 @@ class Day05 : AoCTestBase<Int>(
     testTarget1 = 143,
     puzzleTarget1 = 4814,
     testTarget2 = 123,
-    puzzleTarget2 = null,
+    puzzleTarget2 = 5448,
 ) {
+    private fun isUpdateCorrect(update: List<Int>, rules: List<List<Int>>) = update.indices.none { n ->
+        val after = update.subList(min(update.size, n + 1), update.size).toSet()
+        val before = update.subList(0, n).toSet()
+        val rulesAfter = rules.filter { it.first() == update[n] }.map { it.last() }.filter { it in update }.toSet()
+        val rulesBefore = rules.filter { it.last() == update[n] }.map { it.first() }.filter { it in update }.toSet()
+
+        !(after.containsAll(rulesAfter) && before.containsAll(rulesBefore))
+    }
+
     override fun part1(input: List<String>): Int {
         val rules = input.filter { it.contains("|") }.map { it.ints() }
         val updates = input.filter { it.contains(",") }.map { it.ints() }
 
-        var sum = 0
-
-        updates.forEach { update ->
-            var incorrect = 0
-
-            update.indices.forEach { n ->
-                val rulesAfter = rules.filter { it.first() == update[n] }.map { it.last() }.filter { it in update }
-
-                val correctAfter = update.subList(min(update.size, n + 1), update.size).filter { it in rulesAfter }
-
-                val rulesBefore = rules.filter { it.last() == update[n] }.map { it.first() }.filter { it in update }
-
-                val correctBefore = update.subList(0, n).filter { it in rulesBefore }
-
-                if(rulesAfter.size != correctAfter.size || rulesBefore.size != correctBefore.size)
-                    incorrect ++
-            }
-
-            if(incorrect == 0)
-                sum += update[update.size/2]
-        }
-
-        return sum
+        return updates
+            .filter { isUpdateCorrect(it, rules) }
+            .sumOf { it[it.size / 2] }
     }
 
     override fun part2(input: List<String>): Int {
         val rules = input.filter { it.contains("|") }.map { it.ints() }
         val updates = input.filter { it.contains(",") }.map { it.ints() }
 
-
-
-        val inccrrect = mutableListOf<List<Int>>()
-
-        updates.forEach { update ->
-            var incorrectCounter = 0
-
-            update.indices.forEach { n ->
-                val rulesAfter = rules.filter { it.first() == update[n] }.map { it.last() }.filter { it in update }
-
-                val correctAfter = update.subList(min(update.size, n + 1), update.size).filter { it in rulesAfter }
-
-                val rulesBefore = rules.filter { it.last() == update[n] }.map { it.first() }.filter { it in update }
-
-                val correctBefore = update.subList(0, n).filter { it in rulesBefore }
-
-                if(rulesAfter.size != correctAfter.size || rulesBefore.size != correctBefore.size)
-                    incorrectCounter++
-            }
-
-            if(incorrectCounter > 0)
-                inccrrect.add(update)
-        }
-
-        var sum = 0
-
-        inccrrect.forEach { update ->
+        return updates.filter { !isUpdateCorrect(it, rules) }.sumOf { update ->
             val ordered = mutableListOf<Int>()
 
             update.forEach { n ->
                 val hasToBeBefore = rules.filter { it.first() == n }.map { it.last() }.filter { it in update }.toSet()
                 val hasToBeAfter = rules.filter { it.last() == n }.map { it.last() }.filter { it in update }.toSet()
 
-                for (insertAt in 0..ordered.size){
+                for (insertAt in 0..ordered.size) {
                     val after = ordered.subList(insertAt, ordered.size).toSet()
                     val before = ordered.subList(0, insertAt).toSet()
 
-                    if(after.intersect(hasToBeBefore).size == 0 && before.intersect(hasToBeAfter).size == 0) {
+                    if (after.intersect(hasToBeBefore).isEmpty() && before.intersect(hasToBeAfter).isEmpty()) {
                         ordered.add(insertAt, n)
+
                         break
                     }
                 }
             }
 
-            sum += ordered[ordered.size / 2]
+            ordered[ordered.size / 2]
         }
-
-        return sum
     }
 }

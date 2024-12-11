e5a3f3bb3ad425a45685910572f782ae1ee61d74
Marcin Kotz <marcin@kotz.pl>
2024-12-11 09:09:21 +0100

    feat: day 11, converted to memoized

--- src/com/er453r/codingpuzzles/utils/MemoizationUtils.kt
+++ src/com/er453r/codingpuzzles/utils/MemoizationUtils.kt
@@ -5,6 +5,13 @@ fun <A, O> memoize(function: ((A) -> O).(A) -> O): (A) -> O =
         override fun invoke(a: A): O = invokeCached(Params1(a))
         override fun doInvoke(p: Params1<A>): O = function(this, p.a)
     }
+
+fun <A, B, O> memoize(function: ((A, B) -> O).(A, B) -> O): (A, B) -> O =
+    object : MemoizedFunction<Params2<A, B>, O>(), (A, B) -> O {
+        override fun invoke(a: A, b: B): O = invokeCached(Params2(a, b))
+        override fun doInvoke(p: Params2<A, B>): O = function(this, p.a, p.b)
+    }
+
 fun <A, B, C, O> memoize(function: ((A, B, C) -> O).(A, B, C) -> O): (A, B, C) -> O =
     object : MemoizedFunction<Params3<A, B, C>, O>(), (A, B, C) -> O {
         override fun invoke(a: A, b: B, c: C): O = invokeCached(Params3(a, b, c))
@@ -20,4 +27,5 @@ private abstract class MemoizedFunction<I, O> {
 }
 
 private data class Params1<A>(val a: A)
+private data class Params2<A, B>(val a: A, val b: B)
 private data class Params3<A, B, C>(val a: A, val b: B, val c: C)
--- test/com/er453r/codingpuzzles/aoc/aoc2024/Day11.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2024/Day11.kt
@@ -2,6 +2,7 @@ package com.er453r.codingpuzzles.aoc.aoc2024
 
 import com.er453r.codingpuzzles.aoc.AoCTestBase
 import com.er453r.codingpuzzles.utils.longs
+import com.er453r.codingpuzzles.utils.memoize
 import org.junit.jupiter.api.DisplayName
 
 @DisplayName("AoC 2024 - Day 11")
@@ -13,37 +14,18 @@ class Day11 : AoCTestBase<Long>(
     testTarget2 = 65601038650482,
     puzzleTarget2 = 237994815702032,
 ) {
-    data class Stone(val value:Long, val blinksLeft:Int)
+    private val blink = memoize<Long, Int, Long> { value, blinksLeft ->
+        when {
+            blinksLeft == 0 -> 1
+            value == 0L -> this(1, blinksLeft - 1)
+            value.toString().length % 2 == 0 -> {
+                value.toString().let { it.chunked(it.length / 2) }.map { it.toLong() }.sumOf { this(it, blinksLeft - 1) }
+            }
 
-    private val cache = mutableMapOf<Stone, Long>()
-
-    private fun blink(stone:Stone):Long{
-        if(stone in cache)
-            return cache[stone]!!
-
-        if(stone.blinksLeft == 0)
-            return 1
-
-        val value = when{
-                stone.value == 0L -> blink(Stone(1, stone.blinksLeft - 1))
-                stone.value.toString().length % 2 == 0 -> blink(Stone(stone.value.toString().substring(0, stone.value.toString().length / 2).toLong(), stone.blinksLeft - 1)) + blink(Stone(stone.value.toString().substring(stone.value.toString().length / 2, stone.value.toString().length).toLong(), stone.blinksLeft - 1))
-                else -> blink(Stone(stone.value * 2024, stone.blinksLeft - 1))
+            else -> this(value * 2024, blinksLeft - 1)
         }
-
-        cache[stone] = value
-
-        return value
     }
 
-    override fun part1(input: List<String>): Long {
-        val stones = input.first().longs()
-
-        return stones.sumOf { blink(Stone(it, 25)) }
-    }
-
-    override fun part2(input: List<String>): Long {
-        val stones = input.first().longs()
-
-        return stones.sumOf { blink(Stone(it, 75)) }
-    }
+    override fun part1(input: List<String>) = input.first().longs().sumOf { blink(it, 25) }
+    override fun part2(input: List<String>) = input.first().longs().sumOf { blink(it, 75) }
 }

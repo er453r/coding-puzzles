3ad90377b5d3087af221b9dbec5f4565032fcccc
Marcin Kotz <marcin@kotz.pl>
2024-12-19 08:39:48 +0100

    feat: aoc2024, day 19

--- test/com/er453r/codingpuzzles/aoc/aoc2024/Day19.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2024/Day19.kt
@@ -1,23 +1,36 @@
 package com.er453r.codingpuzzles.aoc.aoc2024
 
 import com.er453r.codingpuzzles.aoc.AoCTestBase
-import org.junit.jupiter.api.Disabled
+import com.er453r.codingpuzzles.utils.memoize
 import org.junit.jupiter.api.DisplayName
 
 @DisplayName("AoC 2024 - Day 19")
-class Day19 : AoCTestBase<Int>(
+class Day19 : AoCTestBase<Long>(
     year = 2024,
     day = 19,
-    testTarget1 = null,
-    puzzleTarget1 = null,
-    testTarget2 = null,
-    puzzleTarget2 = null,
+    testTarget1 = 6,
+    puzzleTarget1 = 340,
+    testTarget2 = 16,
+    puzzleTarget2 = 717561822679428,
 ) {
-    override fun part1(input: List<String>): Int {
-        return 0
+    private var towels = emptyList<String>()
+
+    private val possible = memoize<String, Long> { pattern ->
+        if (pattern.isEmpty())
+            1
+        else
+            towels.filter { pattern.startsWith(it) }.sumOf { this(pattern.replaceFirst(it, "")) }
     }
 
-    override fun part2(input: List<String>): Int {
-        return 0
+    override fun part1(input: List<String>): Long {
+        towels = input.first().split(", ")
+
+        return input.drop(2).count { possible(it) > 0 }.toLong()
+    }
+
+    override fun part2(input: List<String>): Long {
+        towels = input.first().split(", ")
+
+        return input.drop(2).sumOf { possible(it) }
     }
 }

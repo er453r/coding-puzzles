8c50ce854839cc88fc32af799ab5d4517a3c7be3
Marcin Kotz <marcin@kotz.pl>
2024-12-20 22:51:22 +0100

    feat: aoc2024, day 20, part 2

--- test/com/er453r/codingpuzzles/aoc/aoc2024/Day20.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2024/Day20.kt
@@ -1,7 +1,9 @@
 package com.er453r.codingpuzzles.aoc.aoc2024
 
 import com.er453r.codingpuzzles.aoc.AoCTestBase
-import com.er453r.codingpuzzles.utils.*
+import com.er453r.codingpuzzles.utils.Grid
+import com.er453r.codingpuzzles.utils.GridCell
+import com.er453r.codingpuzzles.utils.aStar
 import org.junit.jupiter.api.DisplayName
 
 @DisplayName("AoC 2024 - Day 20")
@@ -10,91 +12,94 @@ class Day20 : AoCTestBase<Int>(
     day = 20,
     testTarget1 = 44,
     puzzleTarget1 = 1332,
-    testTarget2 = null,
+    testTarget2 = 285,
     puzzleTarget2 = null,
 ) {
-    data class State(val cell: GridCell<Char>, val cheats: Int)
-
     override fun part1(input: List<String>): Int {
         val grid = Grid(input.map { it.toCharArray().toList() })
         val start = grid.data.flatten().first { it.value == 'S' }
         val end = grid.data.flatten().first { it.value == 'E' }
+        val test = input.size < 20
+        val cheatsAllowed = 2
 
         val basePath = aStar(
             start = start,
             isEndNode = { it == end },
-            neighbours = { cell ->
-                cell.neighbours().filter { it.value != '#' }
-            }).second
+            neighbours = { cell -> cell.neighbours().filter { it.value != '#' } },
+        )
 
-        val cheatsUsed = mutableSetOf<GridCell<Char>>()
-        val cheatsCount = mutableMapOf<Int, Int>()
+        val path = basePath.first.dropLast(1)
+        val nonWall = grid.data.flatten().filter { it.value != '#' }
 
-        while(true){
-            val paths = aStarM(
-                start = State(cell = start, cheats = 0),
-                isEndNode = { it.cell == end },
-                neighbours = { state ->
-                    state.cell.neighbours()
-                        .filter { it.value != '#' || (state.cheats < 2) }
-                        .map { State(it, if (it.value == '#') state.cheats + 1 else state.cheats) }
-                })
+        var counter = 0
 
-            val save = basePath - paths.second
+        val cache = mutableMapOf<GridCell<Char>, Int>()
 
-            if(save <= 0)
-                break
+        path.forEachIndexed { cheatStartIndex, cheatStart ->
+            nonWall.filter { it != cheatStart }
+                .map { Pair(it, (it.position - cheatStart.position).manhattan()) }
+                .filter { (_, manhattan) -> manhattan < cheatsAllowed + 1 }
+                .forEach { (cheatEnd, manhattan) ->
+                    val rest = cache.getOrPut(cheatEnd){
+                        aStar(
+                            start = cheatEnd,
+                            isEndNode = { it == end },
+                            neighbours = { cell -> cell.neighbours().filter { it.value != '#' } },
+                        ).second
+                    }
 
-            val cheatsUsedNow = cheat.first.mapNotNull { it.last().cheat }.toSet()
+                    val finalPathLength = cheatStartIndex + manhattan + rest
+                    val saved = basePath.second - finalPathLength
 
-            cheatsUsed += cheatsUsedNow
-            cheatsCount[save] = cheatsUsedNow.size
+                    if ((test && saved > 0) || saved >= 100)
+                        counter++
+                }
         }
 
-        val test = input.size < 20
-
-        return cheatsCount.keys.filter { if(test) it < 100 else it >= 100 }.sumOf { cheatsCount[it]!! }
+        return counter
     }
 
-//    override fun part1(input: List<String>): Int {
-//        val grid = Grid(input.map { it.toCharArray().toList() })
-//        val start = grid.data.flatten().first { it.value == 'S' }
-//        val end = grid.data.flatten().first { it.value == 'E' }
-//
-//        val basePath = aStar(
-//            start = start,
-//            isEndNode = { it == end },
-//            neighbours = { cell ->
-//                cell.neighbours().filter { it.value != '#' }
-//            }).second
-//
-//        val cheatsCount = mutableMapOf<Int, Int>()
-//
-//        grid.data.flatten().filter { it.value == '#' }.forEach { cheat ->
-//            cheat.value = '.'
-//
-//            val path = aStar(
-//                start = start,
-//                isEndNode = { it == end },
-//                neighbours = { cell ->
-//                    cell.neighbours().filter { it.value != '#' }
-//                })
-//
-//            val save = basePath - path.second
-//
-//            if(save > 0){
-//                cheatsCount[save] = cheatsCount.getOrDefault(save, 0) + 1
-//            }
-//
-//            cheat.value = '#'
-//        }
-//
-//        // 5635 too high
-//
-//        return cheatsCount.keys.filter { it >= 100 }.sumOf { cheatsCount[it]!! }
-//    }
-
     override fun part2(input: List<String>): Int {
-        return 0
+        val grid = Grid(input.map { it.toCharArray().toList() })
+        val start = grid.data.flatten().first { it.value == 'S' }
+        val end = grid.data.flatten().first { it.value == 'E' }
+        val test = input.size < 20
+        val cheatsAllowed = 20
+
+        val basePath = aStar(
+            start = start,
+            isEndNode = { it == end },
+            neighbours = { cell -> cell.neighbours().filter { it.value != '#' } },
+        )
+
+        val path = basePath.first.dropLast(1)
+        val nonWall = grid.data.flatten().filter { it.value != '#' }
+
+        var counter = 0
+
+        val cache = mutableMapOf<GridCell<Char>, Int>()
+
+        path.forEachIndexed { cheatStartIndex, cheatStart ->
+            nonWall.filter { it != cheatStart }
+                .map { Pair(it, (it.position - cheatStart.position).manhattan()) }
+                .filter { (_, manhattan) -> manhattan < cheatsAllowed + 1 }
+                .forEach { (cheatEnd, manhattan) ->
+                    val rest = cache.getOrPut(cheatEnd){
+                        aStar(
+                            start = cheatEnd,
+                            isEndNode = { it == end },
+                            neighbours = { cell -> cell.neighbours().filter { it.value != '#' } },
+                        ).second
+                    }
+
+                    val finalPathLength = cheatStartIndex + manhattan + rest
+                    val saved = basePath.second - finalPathLength
+
+                    if ((test && saved >= 50) || saved >= 100)
+                        counter++
+                }
+        }
+
+        return counter
     }
 }

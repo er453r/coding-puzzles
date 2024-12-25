7311ecc370d6c1ddebb4a6c62b1d92753ef58fe9
Marcin Kotz <marcin@kotz.pl>
2024-12-25 09:55:36 +0100

    feat: aoc2024, day 25

--- test/com/er453r/codingpuzzles/aoc/aoc2024/Day25.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2024/Day25.kt
@@ -1,22 +1,31 @@
 package com.er453r.codingpuzzles.aoc.aoc2024
 
 import com.er453r.codingpuzzles.aoc.AoCTestBase
+import com.er453r.codingpuzzles.utils.Grid
+import com.er453r.codingpuzzles.utils.split
 import org.junit.jupiter.api.DisplayName
 
 @DisplayName("AoC 2024 - Day 25")
 class Day25 : AoCTestBase<Int>(
     year = 2024,
     day = 25,
-    testTarget1 = null,
-    puzzleTarget1 = null,
-    testTarget2 = null,
-    puzzleTarget2 = null,
+    testTarget1 = 3,
+    puzzleTarget1 = 3133,
+    testTarget2 = 0,
+    puzzleTarget2 = 0,
 ) {
     override fun part1(input: List<String>): Int {
-        return 0
+        val keys = input.split().filter { it.first().contains('#') }.map { Grid(it.map { c -> c.toCharArray().toList() }).columns().map { c -> c.count { g -> g.value == '#' } } }
+        val locks = input.split().filter { it.last().contains('#') }.map { Grid(it.map { c -> c.toCharArray().toList() }).columns().map { c -> c.count { g -> g.value == '#' } } }
+
+        return keys.sumOf { key ->
+            locks.count { lock ->
+                key.zip(lock) { k, l -> k + l }.all { it < 8 }
+            }
+        }
     }
 
     override fun part2(input: List<String>): Int {
-        return 0
+        return 0 // ho ho ho
     }
 }

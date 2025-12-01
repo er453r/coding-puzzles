0a743244533dfe798982551db69a03a13c0c07a9
Marcin Kotz <marcin@kotz.pl>
2025-12-01 11:47:36 +0100

    feat: implement aoc2025, day 01 solutions for both parts

--- test/com/er453r/codingpuzzles/aoc/aoc2025/Day01.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2025/Day01.kt
@@ -2,21 +2,48 @@ package com.er453r.codingpuzzles.aoc.aoc2025
 
 import com.er453r.codingpuzzles.aoc.AoCTestBase
 import org.junit.jupiter.api.DisplayName
+import kotlin.math.absoluteValue
 
 @DisplayName("AoC 2025 - Day 01")
 class Day01 : AoCTestBase<Int>(
     year = 2025,
     day = 1,
-    testTarget1 = null,
-    puzzleTarget1 = null,
-    testTarget2 = null,
-    puzzleTarget2 = null,
+    testTarget1 = 3,
+    puzzleTarget1 = 962,
+    testTarget2 = 6,
+    puzzleTarget2 = 5782,
 ) {
     override fun part1(input: List<String>): Int {
-        return 0
+        val rotations = input.map { it.replace('L', '-').replace('R', '+').toInt() }
+
+        val size = 100
+
+        var position = 50
+        var zeros = 0
+
+        rotations.forEach { steps ->
+            position = (position + steps) % size
+
+            if (position == 0) zeros++
+        }
+
+        return zeros
     }
 
     override fun part2(input: List<String>): Int {
-        return 0
+        val rotations = input.map { it.replace('L', '-').replace('R', '+').toInt() }
+        val size = 100
+
+        var position = 50
+        var zeros = 0
+
+        rotations.forEach { steps ->
+            val current = if (steps >= 0) position else (size - position) % size // mirror if negative
+
+            zeros += (current + steps.absoluteValue) / size // this many overflows
+            position = (size + position + (steps % size)) % size
+        }
+
+        return zeros
     }
 }

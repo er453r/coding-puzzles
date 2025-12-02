5784f124ca0a29c57acd638a449cf52ae5bf9669
Marcin Kotz <marcin@kotz.pl>
2025-12-02 08:18:41 +0100

    feat: implement aoc2025, day 02 solutions for both parts

--- test/com/er453r/codingpuzzles/aoc/aoc2025/Day02.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2025/Day02.kt
@@ -1,22 +1,38 @@
 package com.er453r.codingpuzzles.aoc.aoc2025
 
 import com.er453r.codingpuzzles.aoc.AoCTestBase
+import com.er453r.codingpuzzles.utils.longs
 import org.junit.jupiter.api.DisplayName
 
 @DisplayName("AoC 2025 - Day 02")
-class Day02 : AoCTestBase<Int>(
+class Day02 : AoCTestBase<Long>(
     year = 2025,
     day = 2,
-    testTarget1 = null,
-    puzzleTarget1 = null,
-    testTarget2 = null,
-    puzzleTarget2 = null,
+    testTarget1 = 1227775554,
+    puzzleTarget1 = 38310256125,
+    testTarget2 = 4174379265,
+    puzzleTarget2 = 58961152806,
 ) {
-    override fun part1(input: List<String>): Int {
-        return 0
+    fun isValid(n: Long, limit: Int = 0): Boolean {
+        val nString = n.toString()
+        val end = if (limit == 0) nString.length else limit
+
+        for (i in 2..end)
+            if (nString.length % i == 0)
+                if (nString.take(nString.length / i).repeat(i) == nString)
+                    return false
+
+        return true
     }
 
-    override fun part2(input: List<String>): Int {
-        return 0
-    }
+    fun solve(input: List<String>, limit: Int = 0) = input.joinToString("").split(",")
+        .map { it.replace("-", " ").longs() }
+        .map { Pair(it[0], it[1]) }
+        .flatMap { it.first..it.second }
+        .filter { !isValid(it, limit) }
+        .sum()
+
+    override fun part1(input: List<String>) = solve(input, 2)
+
+    override fun part2(input: List<String>) = solve(input)
 }

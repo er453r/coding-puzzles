b9dde2e4edd0ede28bff9191a61eadc468bcd8ec
Marcin Kotz <marcin@kotz.pl>
2025-12-03 07:54:01 +0100

    refactor: extract common logic for Day 03 solutions into a shared method

--- test/com/er453r/codingpuzzles/aoc/aoc2025/Day03.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2025/Day03.kt
@@ -13,22 +13,10 @@ class Day03 : AoCTestBase<Long>(
     testTarget2 = 3121910778619,
     puzzleTarget2 = 167549941654721,
 ) {
-    override fun part1(input: List<String>): Long {
-        return input.sumOf {
-            val indexed = it.toCharArray().withIndex().toList()
-
-            val firstChar = indexed.dropLast(1).maxBy { it.value }
-            val lastChar = indexed.drop(firstChar.index + 1).maxBy { it.value }
-
-            firstChar.value.digitToInt() * 10L + lastChar.value.digitToInt()
-        }
-    }
-
-    override fun part2(input: List<String>): Long {
-        return input.sumOf {
-            val indexed = it.toCharArray().withIndex().toList()
-
-            var digitsLeft = 11
+    fun solve(input: List<String>, digits:Int): Long {
+        return input.sumOf { line ->
+            val indexed = line.toCharArray().withIndex().toList()
+            var digitsLeft = digits - 1
             var lastIndex = 0
             var value = 0L
 
@@ -45,4 +33,8 @@ class Day03 : AoCTestBase<Long>(
             value
         }
     }
+
+    override fun part1(input: List<String>) = solve(input, 2)
+
+    override fun part2(input: List<String>) = solve(input, 12)
 }

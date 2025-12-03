ec71c23707912698c6d4a86fae4c6ff25b8681f2
Marcin Kotz <marcin@kotz.pl>
2025-12-03 07:50:54 +0100

    feat: implement aoc2025, day 03 solutions for both parts

--- test/com/er453r/codingpuzzles/aoc/aoc2025/Day03.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2025/Day03.kt
@@ -1,22 +1,48 @@
 package com.er453r.codingpuzzles.aoc.aoc2025
 
 import com.er453r.codingpuzzles.aoc.AoCTestBase
+import com.er453r.codingpuzzles.utils.pow
 import org.junit.jupiter.api.DisplayName
 
 @DisplayName("AoC 2025 - Day 03")
-class Day03 : AoCTestBase<Int>(
+class Day03 : AoCTestBase<Long>(
     year = 2025,
     day = 3,
-    testTarget1 = null,
-    puzzleTarget1 = null,
-    testTarget2 = null,
-    puzzleTarget2 = null,
+    testTarget1 = 357L,
+    puzzleTarget1 = 16858,
+    testTarget2 = 3121910778619,
+    puzzleTarget2 = 167549941654721,
 ) {
-    override fun part1(input: List<String>): Int {
-        return 0
+    override fun part1(input: List<String>): Long {
+        return input.sumOf {
+            val indexed = it.toCharArray().withIndex().toList()
+
+            val firstChar = indexed.dropLast(1).maxBy { it.value }
+            val lastChar = indexed.drop(firstChar.index + 1).maxBy { it.value }
+
+            firstChar.value.digitToInt() * 10L + lastChar.value.digitToInt()
+        }
     }
 
-    override fun part2(input: List<String>): Int {
-        return 0
+    override fun part2(input: List<String>): Long {
+        return input.sumOf {
+            val indexed = it.toCharArray().withIndex().toList()
+
+            var digitsLeft = 11
+            var lastIndex = 0
+            var value = 0L
+
+            while(digitsLeft >= 0){
+                val maxChar = indexed.dropLast(digitsLeft).drop(lastIndex ).maxBy { it.value }
+
+                lastIndex = maxChar.index + 1
+
+                value += 10.pow(digitsLeft) * maxChar.value.digitToInt()
+
+                digitsLeft--
+            }
+
+            value
+        }
     }
 }

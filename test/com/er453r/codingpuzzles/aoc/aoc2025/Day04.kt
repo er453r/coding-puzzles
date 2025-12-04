d2539a474454fc995b985cf378c653ef6c210ff0
Marcin Kotz <marcin@kotz.pl>
2025-12-04 08:22:33 +0100

    feat: implement AoC 2025, Day 04 solutions for both parts

--- test/com/er453r/codingpuzzles/aoc/aoc2025/Day04.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2025/Day04.kt
@@ -1,22 +1,43 @@
 package com.er453r.codingpuzzles.aoc.aoc2025
 
 import com.er453r.codingpuzzles.aoc.AoCTestBase
+import com.er453r.codingpuzzles.utils.Grid
 import org.junit.jupiter.api.DisplayName
 
 @DisplayName("AoC 2025 - Day 04")
 class Day04 : AoCTestBase<Int>(
     year = 2025,
     day = 4,
-    testTarget1 = null,
-    puzzleTarget1 = null,
-    testTarget2 = null,
-    puzzleTarget2 = null,
+    testTarget1 = 13,
+    puzzleTarget1 = 1493,
+    testTarget2 = 43,
+    puzzleTarget2 = 9194,
 ) {
     override fun part1(input: List<String>): Int {
-        return 0
+        return Grid(input.map { it.toCharArray().toList() }).data.flatten().count {
+            it.value == '@' && it.neighbours8().count { n -> n.value == '@' } < 4
+        }
     }
 
     override fun part2(input: List<String>): Int {
-        return 0
+        val cells = Grid(input.map { it.toCharArray().toList() }).data.flatten()
+
+        var removed = 0
+
+        while(true){
+            val toBeRemoved = cells.filter {
+                it.value == '@' && it.neighbours8().count { n -> n.value == '@' } < 4
+            }
+
+            if(toBeRemoved.isEmpty())
+                break
+
+            toBeRemoved.forEach {
+                it.value = '.'
+                removed++
+            }
+        }
+
+        return removed
     }
 }

1edb5a85b39a94dba9b76f5ab572bd6c9058d6d0
Marcin Kotz <marcin@kotz.pl>
2025-12-04 08:23:42 +0100

    feat: added AoC 2025, Day 05 template

---
+++ test/com/er453r/codingpuzzles/aoc/aoc2025/Day05.kt
@@ -0,0 +1,22 @@
+package com.er453r.codingpuzzles.aoc.aoc2025
+
+import com.er453r.codingpuzzles.aoc.AoCTestBase
+import org.junit.jupiter.api.DisplayName
+
+@DisplayName("AoC 2025 - Day 05")
+class Day05 : AoCTestBase<Int>(
+    year = 2025,
+    day = 5,
+    testTarget1 = null,
+    puzzleTarget1 = null,
+    testTarget2 = null,
+    puzzleTarget2 = null,
+) {
+    override fun part1(input: List<String>): Int {
+        return 0
+    }
+
+    override fun part2(input: List<String>): Int {
+        return 0
+    }
+}

a121c8be95383ddf4a3080d007da79b382c0ef9f
Marcin Kotz <marcin@kotz.pl>
2025-12-07 10:35:26 +0100

    refactor: optimize grid traversal logic for AoC 2025, Day 07

--- test/com/er453r/codingpuzzles/aoc/aoc2025/Day07.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2025/Day07.kt
@@ -2,7 +2,6 @@ package com.er453r.codingpuzzles.aoc.aoc2025
 
 import com.er453r.codingpuzzles.aoc.AoCTestBase
 import com.er453r.codingpuzzles.utils.Grid
-import com.er453r.codingpuzzles.utils.GridCell
 import com.er453r.codingpuzzles.utils.Vector2d
 import com.er453r.codingpuzzles.utils.memoize
 import org.junit.jupiter.api.Disabled
@@ -18,33 +17,21 @@ class Day07 : AoCTestBase<Long>(
     testTarget2 = 40,
     puzzleTarget2 = 422102272495018,
 ) {
+    val splits = listOf(Vector2d.LEFT, Vector2d.RIGHT)
+
     override fun part1(input: List<String>): Long {
         val grid = Grid(input.map { it.toCharArray().toList() })
-        val start = grid.data.flatten().first { it.value == 'S' }
-        var beams = mutableSetOf(start)
-        val splits = listOf(Vector2d.DOWN + Vector2d.LEFT, Vector2d.DOWN + Vector2d.RIGHT)
-
+        val start = grid.data.flatten().first { it.value == 'S' }.position
+        var beams = setOf(start)
         var count = 0L
 
-        main@ while (true) {
-            val newBeams = mutableSetOf<GridCell<Char>>()
-
-            for (beam in beams) {
-                if (!grid.contains(beam.position + Vector2d.DOWN))
-                    break@main
-
-                val down = grid[beam.position + Vector2d.DOWN]
-
-                if (down.value == '^') {
+        while (grid.contains(beams.first())) {
+            beams = beams.flatMap { beam ->
+                if (grid[beam].value == '^') {
                     count++
-
-                    newBeams += splits.filter { grid.contains(beam.position + it) }
-                        .map { grid[beam.position + it] }
-                } else
-                    newBeams += down
-            }
-
-            beams = newBeams
+                    splits.map { beam + it }
+                } else setOf(beam)
+            }.map { it + Vector2d.DOWN }.toSet()
         }
 
         return count
@@ -53,7 +40,6 @@ class Day07 : AoCTestBase<Long>(
     override fun part2(input: List<String>): Long {
         val grid = Grid(input.map { it.toCharArray().toList() })
         val start = grid.data.flatten().first { it.value == 'S' }.position
-        val splits = listOf(Vector2d.LEFT, Vector2d.RIGHT)
 
         val beam = memoize<Vector2d, Long> { position ->
             when {

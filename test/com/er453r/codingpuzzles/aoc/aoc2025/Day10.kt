0c9a35ca9dd53d8c08bc567ca558606e0c222072
Marcin Kotz <marcin@kotz.pl>
2025-12-11 08:34:28 +0100

    feat: implement solutions and enable tests for AoC 2025, Day 11 (both parts)

--- test/com/er453r/codingpuzzles/aoc/aoc2025/Day10.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2025/Day10.kt
@@ -4,6 +4,7 @@ import com.er453r.codingpuzzles.aoc.AoCTestBase
 import com.er453r.codingpuzzles.utils.aStar
 import com.er453r.codingpuzzles.utils.ints
 import org.junit.jupiter.api.DisplayName
+import kotlin.math.min
 
 @DisplayName("AoC 2025 - Day 10")
 class Day10 : AoCTestBase<Int>(
@@ -18,13 +19,13 @@ class Day10 : AoCTestBase<Int>(
         val line = input.map {
             val parts = it.split(" ")
 
-            val state = parts.first().replace("[", "").replace("]", "").toCharArray().map { it == '#' }
-            val buttons = parts.drop(1).dropLast(1).map { it.ints() }
+            val state = parts.first().replace("[", "").replace("]", "").toCharArray().map { c -> c == '#' }
+            val buttons = parts.drop(1).dropLast(1).map { str -> str.ints().toSet() }.toSet()
 
             Pair(state, buttons)
         }
 
-        return line.map { (endState, buttons) ->
+        return line.sumOf { (endState, buttons) ->
             val presses = aStar(
                 start = endState.map { false },
                 isEndNode = { it == endState },
@@ -32,7 +33,7 @@ class Day10 : AoCTestBase<Int>(
                     buttons.map {
                         val clone = state.toMutableList()
 
-                        it.forEach { clone[it] = !clone[it] }
+                        it.forEach { slot -> clone[slot] = !clone[slot] }
 
                         clone
                     }
@@ -40,7 +41,7 @@ class Day10 : AoCTestBase<Int>(
             )
 
             presses.first.size - 1
-        }.sum()
+        }
     }
 
     override fun part2(input: List<String>): Int {
@@ -48,29 +49,43 @@ class Day10 : AoCTestBase<Int>(
             val parts = it.split(" ")
 
             val state = parts.last().replace("{", "").replace("}", "").ints()
-            val buttons = parts.drop(1).dropLast(1).map { it.ints() }
+            val buttons = parts.drop(1).dropLast(1).map { str -> str.ints().toSet() }.toSet()
 
             Pair(state, buttons)
         }
 
-        return line.map { (endState, buttons) ->
+        return line.sumOf { (endState, buttons) ->
+            val end = endState.map { 0 }
+
             val presses = aStar(
-                start = endState.map { 0 },
-                isEndNode = { it == endState },
+                start = endState,
+                isEndNode = { it == end },
                 neighbours = { state ->
-                    buttons.map { button ->
+                    val zeros = state.indices.filter { state[it] == 0 }.toSet()
+
+                    buttons.filter { it.intersect(zeros).isEmpty() }.map { button ->
                         val clone = state.toMutableList()
 
-                        button.forEach { clone[it]++ }
+                        button.forEach { clone[it]-- }
 
                         clone
-                    }.filter { clone ->
-                        clone.indices.all{ clone[it] <= endState[it] }
+                    }
+                },
+                moveCost = { from, to ->
+                    if(to == end)
+                        1
+                    else{
+                        val minSlot = to.indices.filter { to[it] != 0 }.minBy { to[it] }
+                        val slots = from.indices.filter { from[it] - to[it] > 0 }.toSet()
+
+                        if(slots.contains(minSlot)) 1 else 100
                     }
                 }
             )
 
+            println("derp")
+
             presses.first.size - 1
-        }.sum()
+        }
     }
 }
--- test/com/er453r/codingpuzzles/aoc/aoc2025/Day11.kt
+++ test/com/er453r/codingpuzzles/aoc/aoc2025/Day11.kt
@@ -1,22 +1,50 @@
 package com.er453r.codingpuzzles.aoc.aoc2025
 
 import com.er453r.codingpuzzles.aoc.AoCTestBase
+import com.er453r.codingpuzzles.utils.memoize
+import com.er453r.codingpuzzles.utils.split
 import org.junit.jupiter.api.DisplayName
 
 @DisplayName("AoC 2025 - Day 11")
-class Day11 : AoCTestBase<Int>(
+class Day11 : AoCTestBase<Long>(
     year = 2025,
     day = 11,
-    testTarget1 = null,
-    puzzleTarget1 = null,
-    testTarget2 = null,
-    puzzleTarget2 = null,
+    testTarget1 = 5,
+    puzzleTarget1 = 786,
+    testTarget2 = 2,
+    puzzleTarget2 = 495845045016588,
 ) {
-    override fun part1(input: List<String>): Int {
-        return 0
+    fun Map<String, List<String>>.countPaths(from: String, to: String): Long {
+        val map = this
+
+        val count = memoize<String, Long> { node ->
+            when (node) {
+                to -> 1
+                "out" -> 0
+                else -> map[node]!!.sumOf { this(it) }
+            }
+        }
+
+        return count(from)
     }
 
-    override fun part2(input: List<String>): Int {
-        return 0
+    fun createMap(input: List<String>) = input.associate {
+        val parts = it.split(":")
+        val from = parts.first()
+        val to = parts.last().trim().split(" ")
+
+        from to to
+    }
+
+    override fun part1(input: List<String>) = createMap(input.split().first()).countPaths("you", "out")
+
+    override fun part2(input: List<String>): Long {
+        val map = createMap(input.split().last())
+
+        val svr2fft = map.countPaths("svr", "fft")
+        val fft2dac = map.countPaths("fft", "dac")
+        val dac2out = map.countPaths("dac", "out")
+
+        return svr2fft * fft2dac * dac2out
     }
 }
